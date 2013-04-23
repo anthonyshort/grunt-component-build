53368844cc42d1352cac777d4feabce7cda2f681
Justin Deal <justin.deal@gmail.com>
2013-04-23 10:53:35 -0500

    .conf changed to .config & fixed path for plugins.
    builder.js 0.7.0 changed .conf to .config:
    https://github.com/component/builder.js/commit/f5289e3fe101a053f393bfc92ce8ffef81da20a3
    
    The path of templates and coffee files was being used to read the file, but the path
    needs to be relative to the component. Used path.resolve and path.join for this.

--- plugins/templates.js
+++ plugins/templates.js
@@ -4,10 +4,11 @@ var fs = require('fs');
 
 module.exports = function(builder) {
   builder.hook('before scripts', function(pkg){
-    if( !pkg.conf.templates ) return pkg;
-    pkg.conf.templates.forEach(function(file){
+    if( !pkg.config.templates ) return pkg;
+    pkg.config.templates.forEach(function(file){
       if (path.extname(file) !== '.html') return;
-      var str = fs.readFileSync(file, 'utf8');
+      var filePath = path.resolve(path.join(pkg.dir, file));
+      var str = fs.readFileSync(filePath, 'utf8');
       var compiled = strtojs(str);
       pkg.addFile('scripts', file.replace('.html','.js'), compiled);
     });
